d6170c449fd7456ab63e05347a4f3fdfe41bde44
544146 <wee.tommy99@gmail.com>
2022-12-04 00:26:59 +0000

    Fix path of comment mock

--- tests/test_handle_comment.py
+++ tests/test_handle_comment.py
@@ -6,7 +6,7 @@ from _twitch.sync import Sync
 
 
 def setup_comment():
-    comment_mock = open("../testing/comment_mock", "rb")
+    comment_mock = open("testing/comment_mock", "rb")
     return pickle.load(comment_mock)
 
 
